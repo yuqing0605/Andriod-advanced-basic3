1e98e3a8dfd9a1a78d36db4fb722d3c1b76d9dce
LinYuChhing <linyuching@gmail.com>
2021-11-05 20:25:03 +0800

    first upload

---
+++ basic3/app/src/main/java/com/example/basic3/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.basic3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    int mode = 0;
+
+    public void conversion(View view){
+        EditText editText = findViewById(R.id.editTextTextPersonName);
+        TextView textView = findViewById(R.id.textView);
+        TextView textView2 = findViewById(R.id.textView2);
+
+        try {
+            float temperature = Float.parseFloat(editText.getText().toString());
+            if(!editText.getText().toString().contains("f")&&!editText.getText().toString().contains("F")){
+                if (mode == 0){
+                    textView.setText("華氏溫度:");
+                    textView2.setText("°F");
+                    temperature = temperature*9/5+32;
+                    editText.setText(""+(float)(Math.round(temperature*100))/100);
+                    mode = 1;
+                }
+                else{
+                    textView.setText("攝氏溫度:");
+                    textView2.setText("°C");
+                    temperature = (temperature-32)/9*5;
+                    editText.setText(""+(float)(Math.round(temperature*100))/100);
+                    mode = 0;
+                }
+            }
+            else {
+                Toast.makeText(this, "請輸入數字", Toast.LENGTH_LONG).show();
+            }
+        }
+        catch (Exception e){
+            Toast.makeText(this, "請輸入數字", Toast.LENGTH_LONG).show();
+        }
+    }
+}
